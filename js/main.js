bd7897f7be04650d06ae18b3a6e4498d39200b17
hamady <hamadym909@gmail.com>
2020-11-15 11:56:56 +0200

    second version of droom suit department

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('.groom-suit-looks').owlCarousel({
     loop:true,
     margin:10,
     responsive:{
@@ -10,6 +10,21 @@ $('.owl-carousel').owlCarousel({
         },
         1000:{
             items:3
+        }
+    }
+})
+$('.groom-suit-sellers').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
         }
     }
 })
