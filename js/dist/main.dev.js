a935eec669b93a023c4693583c023dcff8a67cc0
Omar Assem <omar.elghazaly@gampltd.com>
2020-11-16 13:17:05 +0200

    Cleaning and organizing code

---
+++ js/dist/main.dev.js
@@ -0,0 +1,46 @@
+"use strict";
+
+$('.groom-suit-looks').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
+$('.groom-suit-sellers').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 2
+    }
+  }
+});
+$('.groom-suit-looks').owlCarousel({
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 5
+    }
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
 $('.groom-suit-looks').owlCarousel({
-    loop:true,
-    margin:10,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:3
+        1000: {
+            items: 3
         }
     }
 })
 $('.groom-suit-sellers').owlCarousel({
-    loop:true,
-    margin:10,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:2
+        1000: {
+            items: 2
         }
     }
 })
 $('.groom-suit-looks').owlCarousel({
-    loop:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:5
+        1000: {
+            items: 5
         }
     }
 })
